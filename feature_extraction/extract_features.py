529761da46845c3d398d71963aa2285391338931
Thanh-Hoang Nguyen-Vo <nvthoang@gmail.com>
2021-12-03 02:33:28 +1300

    extract features

---
+++ feature_extraction/extract_features.py
@@ -0,0 +1,23 @@
+from feature_extractor.feature_extract import *
+import pandas as pd
+import numpy as np
+
+datasets = ['train', 'val', 'test']
+for dataset in datasets:
+    data = pd.read_csv("../data/original_data/split_by_kmean/data_{}.csv".format(dataset))['Smiles'].to_list()
+    label = pd.read_csv("../data/original_data/split_by_kmean/data_{}.csv".format(dataset))['label']
+    np.save('../data/featurised_data/mol2vec_{}.npy'.format(dataset), Mol2Vec_extract(data))
+    np.save('../data/featurised_data/fp1024_{}.npy'.format(dataset), fp_extract(data, 1024))
+    np.save('../data/featurised_data/fp2048_{}.npy'.format(dataset), fp_extract(data, 2048))
+    np.save('../data/featurised_data/label_{}.npy'.format(dataset), label)
+    #--------------------------------
+    rdkit_data = pd.read_csv("../data/rdkit_md/rdkit_md_{}.csv".format(dataset))
+    np.save('../data/featurised_data/rdkit_md_{}.npy'.format(dataset), rdkit_data)
+    
+
+data = pd.read_csv("../data/external_data/knapsack_refined.csv")['SMILES'].to_list()
+np.save('../data/featurised_data/mol2vec_knapsack.npy', Mol2Vec_extract(data))
+np.save('../data/featurised_data/fp1024_knapsack.npy', fp_extract(data, 1024))
+np.save('../data/featurised_data/fp2048_knapsack.npy', fp_extract(data, 2048))
+rdkit_data = pd.read_csv("../data/rdkit_md/rdkit_md_knapsack.csv")
+np.save('../data/featurised_data/rdkit_md_knapsack.npy', rdkit_data)
