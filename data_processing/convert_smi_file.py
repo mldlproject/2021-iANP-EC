89a7d3ee38465200748b8afe84f0c6ba5e86f287
Thanh-Hoang Nguyen-Vo <nvthoang@gmail.com>
2021-12-03 03:07:36 +1300

    convert csv to smi

--- data_processing/convert_smi_file.py
+++ data_processing/convert_smi_file.py
@@ -7,5 +7,5 @@ import pandas as pd
 for i in range(10):
     dataset = ['train', 'val', 'test']
     for d in dataset:
-        smiles_list = pd.read_csv("../data/ten_seeds/seed_{}/original_data/data_{}.csv".format(i, d), encoding='utf-8')['Smiles'].tolist()
-        convert2smi(smiles_list, tag=d, output_path='../data/ten_seeds/seed_{}/original_data'.format(i))
+        smiles_list = pd.read_csv("../data/ten_seeds/seed_{}/refined_data/data_{}.csv".format(i, d), encoding='utf-8')['Smiles'].tolist()
+        convert2smi(smiles_list, tag=d, output_path='../data/ten_seeds/seed_{}/refined_data'.format(i))
