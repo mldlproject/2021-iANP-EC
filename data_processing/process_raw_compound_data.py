9c670c81334b03812e450d60950f23208d181812
Thanh-Hoang Nguyen-Vo <nvthoang@gmail.com>
2021-12-03 03:45:42 +1300

    refine compound data

--- data_processing/process_raw_compound_data.py
+++ data_processing/process_raw_compound_data.py
@@ -1,43 +1,27 @@
 # Import libraries
-import numpy as np 
 import pandas as pd 
-from rdkit import Chem
+from qsarct.qsarct import *
 
 #============================================================
 # Load active/inactive compounds
-active = pd.read_csv("data/AnticancerNP_active.csv")
-inactive = pd.read_csv("data/AnticancerNP_inactive.csv")
+active_compounds   = pd.read_csv("../data/original_data/AnticancerNP_active.csv")['SMILES'].tolist()
+inactive_compounds = pd.read_csv("../data/original_data/AnticancerNP_inactive.csv")['SMILES'].tolist()
 
-#------------------------------------------------------------
-# Remove dupplicates
-filtered_active   = active.drop_duplicates(subset=['SMILES'], keep='first')
-filtered_inactive = inactive.drop_duplicates(subset=['SMILES'], keep='first')
+# Fullly refine
+refined_actives   = full_refine(active_compounds)
+refined_inactives = full_refine(inactive_compounds)
 
-#------------------------------------------------------------
-# Check validity
-canonical_active = [] # For active compounds
-for smiles in filtered_active['SMILES'].tolist():
-    canonical_smiles = Chem.MolToSmiles(Chem.MolFromSmiles(smiles))
-    canonical_active.append(canonical_smiles) 
-df_canonical_active = pd.DataFrame(canonical_active, columns=["SMILES"])
-filtered_canonical_active = df_canonical_active.drop_duplicates(subset=['SMILES'], keep='first')
+# Remove conflicted labels
+active_labels   = [1]*len(refined_actives)
+inactive_labels = [0]*len(refined_inactives)
 
+refined_compounds = refined_actives + refined_inactives   
+refined_labels    = [1]*len(refined_actives) + [0]*len(refined_inactives)   
 
-canonical_inactive = [] # For inactive compounds
-for smiles in filtered_inactive['SMILES'].tolist():
-    canonical_smiles = Chem.MolToSmiles(Chem.MolFromSmiles(smiles))
-    canonical_inactive.append(canonical_smiles) 
-df_canonical_inactive = pd.DataFrame(canonical_inactive, columns=["SMILES"])
-filtered_canonical_inactive = df_canonical_inactive.drop_duplicates(subset=['SMILES'], keep='first')
+df = pd.DataFrame(zip(refined_compounds, refined_labels), columns=['SMILES', 'class'])
+df = df.drop_duplicates(subset=['SMILES'], keep=False)
+df.to_csv("../data/refined_data/refined_anticancerNP.csv", index=False)
 
-#------------------------------------------------------------
-# Refine dataset
-filtered_canonical_active['Label']   = np.ones(len(filtered_canonical_active), dtype=int)
-filtered_canonical_inactive['Label'] = np.zeros(len(filtered_canonical_inactive), dtype=int)
-
-filtered_canonical_merged = pd.concat([filtered_canonical_active, filtered_canonical_inactive], axis=0, ignore_index=True)
-refined_canonical_merged = filtered_canonical_merged.drop_duplicates(subset=['SMILES'], keep=False)
-
-num_active   = len(refined_canonical_merged[refined_canonical_merged['Label']==1]) #372
-num_inactive = len(refined_canonical_merged[refined_canonical_merged['Label']==0]) #639
-pd.DataFrame.to_csv(refined_canonical_merged, "./data/refined_anticancerNP.csv", index=False)
+# Check number of compounds per class
+num_active   = len(df[df['class']==1]) #372
+num_inactive = len(df[df['class']==0]) #639
